ad49ab2b30202d243473dc762fe8c614256dfbde
zyh940305858 <940305858@qq.com>
2019-12-27 08:00:05 +0800

    Demo0

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Demo0 from './page/Demo0'
+
+ReactDOM.render(<Demo0 />, document.getElementById('root'));
---
+++ src/page/Demo0.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+// React hooks 纯函数式编程
+function Demo0(){
+    const [ count, setCount ] = useState(0);
+    return (
+        <div>
+            <p>You clicked { count } times </p>
+            <button onClick={()=>setCount(count+1)} >Click me</button>
+        </div>
+    )
+}
+
+// class类形式
+// class Demo0 extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = { count: 0 };
+//     }
+//     render() {
+//         return (
+//             <div>
+//                 <p>count:{ this.state.count }</p>
+//                 <button onClick={this.addfn.bind(this)}>add count</button>
+//             </div>
+//         );
+//     }
+//     addfn(){
+//         this.setState({
+//             count:this.state.count+1
+//         })
+//     }
+// }
+
+export default Demo0;
